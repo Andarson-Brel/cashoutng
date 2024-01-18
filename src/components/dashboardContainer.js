011ee5feea98353ced7eb6ba9e1da3c921aac3e6
kumdan job <redeks123456@gmail.com>
2024-01-18 19:10:46 +0100

    m

--- src/components/dashboardContainer.js
+++
@@ -1,9 +0,0 @@
-function DashboardContainer({ children, width }) {
-  return (
-    <div className="dashboard-card" style={{ width: width }}>
-      {children}
-    </div>
-  );
-}
-
-export default DashboardContainer;
